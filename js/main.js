d3b47bc5786c907829d397080dd3b2c9780dbdc7
Abhishek Singh <abhi8882828@gmail.com>
2024-02-03 10:42:15 +0530

    Update JavaScript and CSS libraries and fix carousel bug

--- js/main.js
+++ js/main.js
@@ -167,7 +167,7 @@ $(document).ready(function () {
     }
   })
 
-  $('.testimonial-carousel').owlCarousel({
+  $('.testimonial-carous').owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
@@ -196,7 +196,6 @@ $(document).ready(function () {
     autoplayHoverPause: true,
     animateOut: 'fadeOut',
     animateIn: 'fadeIn',
-    navText: ["<i class='fa fa-caret-left'></i>", "<i class='fa fa-caret-right'></i>"],
     responsive: {
       0: {
         items: 1
@@ -212,19 +211,19 @@ $(document).ready(function () {
 
   $('.affiliations-carousel').owlCarousel({
     loop: true,
-    margin: 10,
-    nav: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    navText: ["<i class='fa fa-caret-left'></i>", "<i class='fa fa-caret-right'></i>"],
+      center: true,
+      items: 5,
+      margin: 10,
+      autoplayHoverPause: true,
+      dots:true,
+      autoplayTimeout: 8500,
+      smartSpeed: 450,
     responsive: {
       0: {
         items: 2
       },
-      400:{
-        items :3
+      400: {
+        items: 3
       },
       600: {
         items: 3
@@ -234,5 +233,59 @@ $(document).ready(function () {
       }
     }
   })
-
+  
 });
+
+
+jQuery(document).ready(function($) {
+  "use strict";
+  //  TESTIMONIALS CAROUSEL HOOK
+  $('#customers-testimonials').owlCarousel({
+      loop: true,
+      center: true,
+      items: 3,
+      margin: 0,
+      autoplayHoverPause: true,
+      dots:true,
+      autoplayTimeout: 8500,
+      smartSpeed: 450,
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 2
+        },
+        1170: {
+          items: 3
+        }
+      }
+  });
+  
+});
+
+$(document).ready(function() {
+  $('.read-more-btn').each(function() {
+      var $btn = $(this); // The "Read More" button
+      var $text = $btn.prev('.testimonial-text'); // The text container
+      var fullText = $text.text(); // Full text content
+      var shortText = fullText.substr(0, 150) + '...'; // Shortened text
+      
+      // Initially set the text to the shortened version
+      $text.data('full-text', fullText).text(shortText);
+
+      $btn.on('click', function() {
+          // Check if the button is in "Read More" or "Read Less" state
+          if ($btn.data('isExpanded')) {
+              // Collapse the text
+              $text.text(shortText);
+              $btn.text('Read More').data('isExpanded', false);
+          } else {
+              // Expand the text
+              $text.text($text.data('full-text'));
+              $btn.text('Read Less').data('isExpanded', true);
+          }
+      });
+  });
+});
+
